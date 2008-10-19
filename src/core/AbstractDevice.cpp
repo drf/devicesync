57cac939ae2bcf56b36ca06906985d91cb6c53e6
Dario Freddi <drf54321@gmail.com>
2008-10-20 01:17:04 +0200

    Adding abstractdevices
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

---
+++ src/core/AbstractDevice.cpp
@@ -0,0 +1,16 @@
+/*
+ * AbstractDevice.cpp
+ *
+ *  Created on: 20-ott-2008
+ *      Author: drf
+ */
+
+#include "AbstractDevice.h"
+
+AbstractDevice::AbstractDevice()
+{
+}
+
+AbstractDevice::~AbstractDevice()
+{
+}
---
+++ src/core/AbstractDevice.h
@@ -0,0 +1,36 @@
+/*
+ * AbstractDevice.h
+ *
+ *  Created on: 20-ott-2008
+ *      Author: drf
+ */
+
+#ifndef ABSTRACTDEVICE_H
+#define ABSTRACTDEVICE_H
+
+#include <QList>
+
+class AbstractDevice
+{
+    public:
+        typedef QList<AbstractDevice*> List;
+
+        AbstractDevice();
+        virtual ~AbstractDevice();
+};
+
+class DeviceContainer
+{
+    public:
+        typedef QList<DeviceContainer*> List;
+
+        enum Status {
+            Connected,
+            Disconnected
+        };
+
+        AbstractDevice *device;
+        Status status;
+};
+
+#endif /* ABSTRACTDEVICE_H_ */
